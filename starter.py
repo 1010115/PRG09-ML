2bca31554e3e272a7499b7f2d9bdd865abf2d1aa
“Zino” <1010115@hr.nl>
2026-01-29 19:37:26 +0100

    Added guesses for clusters

--- starter.py
+++ starter.py
@@ -13,6 +13,8 @@ Benodigde libraries:
 from machinelearningdata import Machine_Learning_Data
 import numpy as np
 import matplotlib.pyplot as plt
+from matplotlib.patches import Circle
+import pandas as pd
 from sklearn.metrics import accuracy_score
 
 
@@ -21,11 +23,11 @@ def extract_from_json_as_np_array(key, json_data):
     data_as_array = []
     for p in json_data:
         data_as_array.append(p[key])
-
+    
     return np.array(data_as_array)
 
 
-STUDENTNUMMER = "1234567" # TODO: aanpassen aan je eigen studentnummer
+STUDENTNUMMER = "1010115"
 
 assert STUDENTNUMMER != "1234567", "Verander 1234567 in je eigen studentnummer"
 
@@ -33,7 +35,6 @@ print("STARTER CODE")
 
 # maak een data-object aan om jouw data van de server op te halen
 data = Machine_Learning_Data(STUDENTNUMMER)
-
 
 # UNSUPERVISED LEARNING
 
@@ -43,21 +44,31 @@ kmeans_training = data.clustering_training()
 # extract de x waarden
 X = extract_from_json_as_np_array("x", kmeans_training)
 
-#print(X)
+print(X)
 
-# slice kolommen voor plotten (let op, dit is de y voor de y-as, niet te verwarren met een y van de data)
-x = X[...,0]
-y = X[...,1]
+# slice kolommen voor plotten
+# Gebruik de standaard [:, n] syntax voor 2D arrays
+x = X[:, 0]
+y = X[:, 1]
 
 # teken de punten
-for i in range(len(x)):
-    plt.plot(x[i], y[i], 'k.') # k = zwart
+# Vectorized plotting is much faster than a loop
+plt.plot(x, y, 'k.', markersize=2) 
 
 plt.axis([min(x), max(x), min(y), max(y)])
-plt.show()
+#plt.show()
 
 # TODO: print deze punten uit en omcirkel de mogelijke clusters
+guess = np.array([[12,9],[60,65],[80,9],[83,30],[95,65]])
+guessX = guess[...,0]
+guessY = guess[...,1]
 
+ax = plt.gca()
+for center in guess:
+    circle = Circle(center, radius=5, ec="blue", fill=False, linewidth=2)
+    ax.add_patch(circle)
+
+plt.show()
 
 # TODO: ontdek de clusters mbv kmeans en teken een plot met kleurtjes
 
@@ -99,4 +110,3 @@ Z = np.zeros(100) # dit is een gok dat alles 0 is... kan je zelf voorspellen hoe
 # stuur je voorspelling naar de server om te kijken hoe goed je het gedaan hebt
 classification_test = data.classification_test(Z.tolist()) # tolist zorgt ervoor dat het numpy object uit de predict omgezet wordt naar een 'normale' lijst van 1'en en 0'en
 print("Classificatie accuratie (test): " + str(classification_test))
-
