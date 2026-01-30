a0b1bd2817cff1e191ebdff98fd6e595d585ca01
“Zino” <1010115@hr.nl>
2026-01-30 11:38:45 +0100

    added lnreg  Decision boundary and final test set

--- starter.py
+++ starter.py
@@ -72,7 +72,7 @@ plt.figure(figsize=(15, 5))
 
 for i, k_value in enumerate([5, 6, 4],1):
     plt.subplot(1, 3, i)
-    kmeans = KMeans(n_clusters=k_value, n_init='auto').fit(X)
+    kmeans = KMeans(n_clusters=k_value).fit(X)
     labels = kmeans.labels_
     centers = kmeans.cluster_centers_
 
@@ -98,41 +98,56 @@ Y = extract_from_json_as_np_array("y", classification_training)
 
 
 # TODO: leer de classificaties
-# print(X)
+x= X[:,0]
+y= X[:,1]
 
-x = X[:, 0]
-y = X[:, 1]
 plt.figure()
-plt.plot(x,y, 'k.') 
-plt.axis([min(x), max(x), min(y), max(y)])
+plt.scatter(x,y, c=Y) 
+
 # plt.show()
 lnRegression = LogisticRegression().fit(X,Y)
-# print(lnRegression.predict(X))
 
+# Decision boundary
+coef = lnRegression.coef_[0]
+intercept = lnRegression.intercept_[0]
+line_x = np.linspace(min(x),max(x))
+line_y = -(coef[0] * line_x + intercept) / coef[1]
+
+plt.plot(line_x, line_y, color='red')
 
 
 # TODO: voorspel na het trainen de Y-waarden (je gebruikt hiervoor dus niet meer de
 #       echte Y-waarden, maar enkel de X en je getrainde classifier) en noem deze
 #       bijvoordeeld Y_predict
+plt.figure()
 Y_predict = lnRegression.predict(X)
+plt.scatter(x,y, c=Y_predict) 
+
 
 
 # TODO: vergelijk Y_predict met de echte Y om te zien hoe goed je getraind hebt
-print(accuracy_score(Y,Y_predict))
+print("Training accuracy "+ str(accuracy_score(Y,Y_predict)))
+# plt.show()
 
 # haal data op om te testen
 classification_test = data.classification_test()
 # testen doen we 'geblinddoekt' je krijgt nu de Y's niet
 X_test = extract_from_json_as_np_array("x", classification_test)
-
 # TODO: voorspel na nog een keer de Y-waarden, en plaats die in de variabele Z
 #       je kunt nu zelf niet testen hoe goed je het gedaan hebt omdat je nu
 #       geen echte Y-waarden gekregen hebt.
 #       onderstaande code stuurt je voorspelling naar de server, die je dan
 #       vertelt hoeveel er goed waren.
+X_predict = lnRegression.predict(X_test)
+plt.figure()
+plt.scatter(X_test[:,0],X_test[:,1], c=X_predict) 
+plt.plot(line_x, line_y, color='red')
 
-Z = np.zeros(100) # dit is een gok dat alles 0 is... kan je zelf voorspellen hoeveel procent er goed is?
+
+Z = X_predict
 
 # stuur je voorspelling naar de server om te kijken hoe goed je het gedaan hebt
 classification_test = data.classification_test(Z.tolist()) # tolist zorgt ervoor dat het numpy object uit de predict omgezet wordt naar een 'normale' lijst van 1'en en 0'en
 print("Classificatie accuratie (test): " + str(classification_test))
+
+plt.show()
